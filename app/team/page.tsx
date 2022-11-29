5a46712d817da2c4655ad221d3afd2fe2a11526c
anandani4136 <anandani4136@students.d211.org>
2022-11-29 15:34:10 -0600

    Section Segmentation Styling V1.0 :lipstick:

---
+++ app/team/page.tsx
@@ -0,0 +1,50 @@
+import Navbar from "../components/navbar";
+import "./main.scss"
+
+const Page = () => {
+    return (
+        <div>
+            <h1>Sample</h1>
+            <div className="section">
+                <div className="column">
+                    <div className="box"> 
+                        <h2>Column 3</h2>
+                        <p>Some text..</p>              
+                    </div>
+                </div>
+
+                <div className="column">
+                    <div className="box"> 
+                        <h2>Column 3</h2>
+                        <p>Some text..</p>              
+                    </div>
+                </div>
+
+                <div className="column">
+                    <div className="box"> 
+                        <h2>Column 3</h2>
+                        <p>Some text..</p>              
+                    </div>
+                </div>
+                <div className="column">
+                    <div className="box"> 
+                        <h2>Column 3</h2>
+                        <p>Some text..</p>              
+                    </div>
+                </div>
+                <div className="column">
+                    <div className="box"> 
+                        <h2>Column 3</h2>
+                        <p>Some text..</p>              
+                    </div>
+                </div>
+                
+                
+            </div>
+        </div>
+    );
+};
+
+
+
+export default Page;
