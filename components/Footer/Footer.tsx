d316d085cebce6ee9d96b3203850e9ee6308cb2f
Tanmoy Debnath <53893576+tandebnath@users.noreply.github.com>
2023-08-16 16:03:35 -0500

    🔁 Updated footer design (#22)
    * 🔁 Updated footer designs
    
    * 🤚 Handled responsiveness for footer

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -3,8 +3,46 @@ import styles from "./Footer.module.scss";
 const Footer = () => {
     return (
         <footer className={styles.footer}>
-            <span>HackIllinois</span>
-            <span>Privacy Policy</span>
+            <div>
+                <div>
+                    <span>Contact Information</span>
+                    <span>Reach out to us with any inquiries!</span>
+                    <div>
+                        <a
+                            href="mailto:contact@hackillinois.org"
+                            target="_blank"
+                            rel="noopener noreferrer"
+                        >
+                            <img src="/icons/email-border.svg" className={styles.icon} />
+                        </a>
+                        contact@hackillinois.org
+                    </div>
+                </div>
+                <div>
+                    <span>Want to stay up to date?</span>
+                    <span>Follow us on social media!</span>
+                    <div>
+                        <a
+                            href="https://instagram.com/HackIllinois"
+                            target="_blank"
+                            rel="noopener noreferrer"
+                        >
+                            <img src="/icons/instagram-border.svg" className={styles.icon} />
+                        </a>
+                        <a
+                            href="https://www.linkedin.com/company/hackillinois/"
+                            target="_blank"
+                            rel="noopener noreferrer"
+                        >
+                            <img src="/icons/linkedin-border.svg" className={styles.icon} />
+                        </a>
+                    </div>
+                </div>
+            </div>
+            <div>
+                <span>Copyright@2023</span>
+                <span>Privacy Policy</span>
+            </div>
         </footer>
     );
 };
