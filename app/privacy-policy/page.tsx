0c30c96dc9946591cb0d91d91a42c733d4ad0def
Tanmoy Debnath <53893576+tandebnath@users.noreply.github.com>
2023-08-17 18:49:17 -0500

    🎨 Added Privacy Policy art

--- app/privacy-policy/page.tsx
+++ app/privacy-policy/page.tsx
@@ -22,81 +22,95 @@ type privacyPolicySubPoints = {
 
 const PrivacyPolicy = () => {
     return (
-        <Container>
-            {data.map((privacyPolicy: privacyPolicy) => (
-                <div>
-                    <h1 className={styles.mainTitle}>{privacyPolicy.title}</h1>
-                    <div className={styles.mainDescriptionContainer}>
-                        {privacyPolicy.description.map((el: string) => (
-                            <p className={styles.mainDescription}>{el}</p>
-                        ))}
-                    </div>
-                    <div className={styles.pointsContainer}>
-                        {privacyPolicy.points.map(
-                            (privacyPolicyPoint: privacyPolicyPoints) => (
-                                <div className={styles.point}>
-                                    <span className={styles.pointTitle}>
-                                        {privacyPolicyPoint.title}
-                                    </span>
-                                    <p className={styles.pointDescription}>
-                                        {privacyPolicyPoint.description}
-                                    </p>
-                                    {privacyPolicyPoint.subPoints.length >
-                                        0 && (
-                                        <div
-                                            className={
-                                                styles.subPointsContainer
-                                            }
-                                        >
-                                            {privacyPolicyPoint.subPoints.map(
-                                                (
-                                                    privacyPolicySubpoint: privacyPolicySubPoints
-                                                ) => (
-                                                    <div
-                                                        className={
-                                                            styles.subPoint
-                                                        }
-                                                    >
-                                                        <span
+        <div className={styles.privacyPolicy}>
+            <Container compact>
+                {data.map((privacyPolicy: privacyPolicy) => (
+                    <div>
+                        <h1 className={styles.mainTitle}>
+                            {privacyPolicy.title}
+                        </h1>
+                        <div className={styles.mainDescriptionContainer}>
+                            {privacyPolicy.description.map((el: string) => (
+                                <p className={styles.mainDescription}>{el}</p>
+                            ))}
+                        </div>
+                        <div className={styles.pointsContainer}>
+                            {privacyPolicy.points.map(
+                                (privacyPolicyPoint: privacyPolicyPoints) => (
+                                    <div className={styles.point}>
+                                        <span className={styles.pointTitle}>
+                                            {privacyPolicyPoint.title}
+                                        </span>
+                                        <p className={styles.pointDescription}>
+                                            {privacyPolicyPoint.description}
+                                        </p>
+                                        {privacyPolicyPoint.subPoints.length >
+                                            0 && (
+                                            <div
+                                                className={
+                                                    styles.subPointsContainer
+                                                }
+                                            >
+                                                {privacyPolicyPoint.subPoints.map(
+                                                    (
+                                                        privacyPolicySubpoint: privacyPolicySubPoints
+                                                    ) => (
+                                                        <div
                                                             className={
-                                                                styles.subPointTitle
+                                                                styles.subPoint
                                                             }
                                                         >
-                                                            {
-                                                                privacyPolicySubpoint.title
-                                                            }
-                                                        </span>
-                                                        <div>
-                                                            {privacyPolicySubpoint.description.map(
-                                                                (
-                                                                    el: string
-                                                                ) => (
-                                                                    <p
-                                                                        className={
-                                                                            styles.subPointDescription
-                                                                        }
-                                                                    >
-                                                                        {el}
-                                                                    </p>
-                                                                )
-                                                            )}
+                                                            <span
+                                                                className={
+                                                                    styles.subPointTitle
+                                                                }
+                                                            >
+                                                                {
+                                                                    privacyPolicySubpoint.title
+                                                                }
+                                                            </span>
+                                                            <div>
+                                                                {privacyPolicySubpoint.description.map(
+                                                                    (
+                                                                        el: string
+                                                                    ) => (
+                                                                        <p
+                                                                            className={
+                                                                                styles.subPointDescription
+                                                                            }
+                                                                        >
+                                                                            {el}
+                                                                        </p>
+                                                                    )
+                                                                )}
+                                                            </div>
                                                         </div>
-                                                    </div>
-                                                )
-                                            )}
-                                        </div>
-                                    )}
-                                </div>
-                            )
-                        )}
+                                                    )
+                                                )}
+                                            </div>
+                                        )}
+                                    </div>
+                                )
+                            )}
+                        </div>
+                        <div className={styles.lastUpdated}>
+                            <span>Last Updated:</span>
+                            <span>{privacyPolicy.lastUpdatedOn}</span>
+                        </div>
                     </div>
-                    <div className={styles.lastUpdated}>
-                        <span>Last Updated:</span>
-                        <span>{privacyPolicy.lastUpdatedOn}</span>
-                    </div>
-                </div>
-            ))}
-        </Container>
+                ))}
+            </Container>
+            <div className={styles.background}>
+                <img
+                    src="/privacyPolicy/background-left.svg"
+                    className={styles.backgroundLeft}
+                />
+                <img
+                    src="/privacyPolicy/background-right.svg"
+                    className={styles.backgroundLeft}
+                />
+            </div>
+        </div>
     );
 };
 
