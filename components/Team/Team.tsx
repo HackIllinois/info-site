3314b08a6e49e046b936e38282640cf8cddaefe1
j164 <edleyjacob@gmail.com>
2024-08-02 22:36:53 -0500

    add dropdown to select different team years

---
+++ components/Team/Team.tsx
@@ -0,0 +1,101 @@
+"use client";
+import React, { useState } from "react";
+import { data } from "@/modules/TeamData";
+import styles from "./Team.module.scss";
+import Container from "@/components/Container/Container";
+
+type team = {
+    teamName: string;
+    teamMembers: teamMember[];
+};
+
+type teamMember = {
+    name: string;
+    position: string;
+    funFact: string;
+    photo: string;
+    emoji: string;
+};
+
+type year = keyof typeof data;
+
+const Team = () => {
+    const [year, setYear] = useState<year>("2024");
+
+    return (
+        <>
+            <Container>
+                <h1 className={styles.mainTitle}>Meet our <select
+                    value={year}
+                    onChange={(e) => setYear(e.target.value as year)}
+                    className={styles.yearDropdown}
+                >
+                    {Object.keys(data).map((key) => (
+                        <option key={key} value={key}>
+                            {key}
+                        </option>
+                    ))}
+                </select> Team</h1>
+                {data[year].map((team: team, index: React.Key) => (
+                    <div className={styles.teamLayout} key={index}>
+                        <h1>{team.teamName}</h1>
+                        <div className={styles.membersLayout}>
+                            {team.teamMembers.map(
+                                (member: teamMember, key: React.Key) => (
+                                    <div
+                                        className={styles.memberCard}
+                                        key={key}
+                                    >
+                                        <div className={styles.nameAndPosition}>
+                                            <span className={styles.name}>
+                                                {member.name}
+                                            </span>
+                                            <span className={styles.position}>
+                                                {member.position}
+                                            </span>
+                                        </div>
+                                        <div className={styles.emojiAndPhoto}>
+                                            <div
+                                                className={
+                                                    styles.emojiContainer
+                                                }
+                                            >
+                                                <span className={styles.emoji}>
+                                                    {member.emoji}
+                                                </span>
+                                            </div>
+                                            <img
+                                                src={
+                                                    member.photo
+                                                        ? member.photo
+                                                        : "team/default.svg"
+                                                }
+                                                alt={member.name}
+                                                className={styles.photo}
+                                                loading="lazy"
+                                            />
+                                        </div>
+                                        <div className={styles.funFact}>
+                                            <span className={styles.fact}>
+                                                {member.funFact}
+                                            </span>
+                                        </div>
+                                    </div>
+                                )
+                            )}
+                        </div>
+                    </div>
+                ))}
+            </Container>
+            <div className={styles.footerArt}>
+                <img src="team-footer-shapes.svg" className={styles.shapes} />
+                <img
+                    src="team-footer-characters.svg"
+                    className={styles.characters}
+                />
+            </div>
+        </>
+    );
+};
+
+export default Team;
