601603d2affac35adddb4c7eceae9659ec20f9af
Miguel Aenlle <66172204+miguelaenlle@users.noreply.github.com>
2024-10-29 10:36:41 -0500

    Improved Team Year Selector (#46)
    * Implemented enhanced animations on the Team Year selector
    
    * Added functionality to close Team Year Dropdown list by clicking away from the dropdown
    
    * Reduced option size to 3rem on mobile
    
    * Increase vertical padding to 3rem on mobile
    
    * Added x button
    
    * Moved new border radius / shadow to mobile
    
    * Moved x button down
    
    * Fixed alignment of buttons in selector; fixed bug causing dropdown to stay open
    
    * Adjusted styling of dropdown on desktop

--- components/Select/Select.tsx
+++ components/Select/Select.tsx
@@ -1,6 +1,7 @@
 "use client";
-import React, { useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import styles from "./Select.module.scss";
+import clsx from "clsx";
 
 type SelectProps = {
     options: readonly string[];
@@ -20,9 +21,26 @@ const Select: React.FC<SelectProps> = ({
     const [selected, setInnerSelected] = useState(options[0]);
     const [open, setOpen] = useState(false);
 
+    const dropdownRef = useRef<HTMLDivElement | null>(null);
+    const buttonRef =  useRef<HTMLButtonElement | null>(null);
+
+    const closeMenu = (event: MouseEvent) => {
+        if (dropdownRef.current && open && !dropdownRef.current.contains(event.target as Node) && !buttonRef.current?.contains(event.target as Node)) {
+            setOpen(false);
+        }
+    }
+
+    useEffect(() => {
+        document.addEventListener("mousedown", closeMenu);
+        return () => {
+            document.removeEventListener("mousedown", closeMenu);
+        }
+    }, [open]);
+
     return (
         <div className={styles.select}>
             <button
+                ref={buttonRef}
                 onClick={() => setOpen(previous => !previous)}
                 className={`${styles.dropdownBtn} ${
                     open ? styles.openSelect : ""
@@ -30,21 +48,38 @@ const Select: React.FC<SelectProps> = ({
             >
                 <h1 className={titleStyle}>{selected}</h1>
             </button>
-            <div className={styles.dropdown} style={{ opacity: open ? 1 : 0 }}>
-                {options.map(option => (
-                    <button
-                        key={option}
-                        onClick={() => {
-                            setInnerSelected(option);
-                            setSelected(option);
-                            setOpen(false);
-                        }}
-                        className={
-                            option === selected ? selectedStyle : optionStyle
-                        }
-                    >
-                        <p>{option}</p>
-                    </button>
+            <div ref={dropdownRef} className={
+                clsx(
+                    styles.dropdown,
+                    open ? styles.show : styles.hide
+                )
+            }>
+                    <div className={styles.row}>
+                    <div className={styles.closeBtn} onClick={() => {
+                        setOpen(false);
+                    }}>
+                        <span></span>
+                        <span></span>
+                    </div>
+                </div>
+                {options.map((option, index) => (
+                    <>
+                        <button
+                            key={option}
+                            onClick={() => {
+                                setInnerSelected(option);
+                                setSelected(option);
+                                setOpen(false);
+                            }}
+                            className={
+                                option === selected ? selectedStyle : optionStyle
+                            }
+                        >
+                            <p>{option}</p>
+                        </button>
+                        {(index === 0) ? <hr/> : <></>}
+                    </>
+
                 ))}
             </div>
         </div>
